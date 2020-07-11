e38de31204ef7e69ab4bcfbe1c3b5420696412e7
Tj (bougyman) Vanderpoel <tj@rubyists.com>
2020-07-11 22:01:11 +0000

    Adds gitlab ci config and build.sh for it

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Brings in optparse(), die(), and bud()
+# shellcheck source=lib/functions.sh
+. lib/functions.sh
+optparse "$@"
+
+export BASEPKG ARCH REPOSITORY author created_by tag
+# export BUILDAH_FORMAT=docker If we want docker format
+export STORAGE_DRIVER=vfs # Use vfs because overlay on overlay in Docker is whack
+
+export REGISTRY_AUTH_FILE=${HOME}/auth.json # Set registry file location
+echo "$CI_REGISTRY_PASSWORD" | buildah login -u "$CI_REGISTRY_USER" --password-stdin "$CI_REGISTRY" # Login to registry
+
+./buildah.sh
